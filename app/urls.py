9af95e0852ead88c0ca1e3ec0830e0acfe868a6f
memonayaj <memonayaj9864@gmail.com>
2021-11-21 18:45:26 +0530

    main

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('services/', views.services, name='services'),
+    path('faq/', views.faq, name='faq'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('rates/', views.rates, name='rates'),
+    path('reviews/', views.reviews, name='reviews'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -6,3 +6,20 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'index.html')
+
+def faq(request):
+    return render(request,'electrical-faq.html')
+
+
+def rates(request):
+    return render(request,'electrical-rates.html')
+
+def contacts(request):
+    return render(request,'electrical-contact.html')
+
+
+def services(request):
+    return render(request,'electrical-services.html')
+
+def reviews(request):
+    return render(request,'electrical-reviews.html')
