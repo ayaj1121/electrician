9a06eb72f01fd009488a141811bf42ecbe182434
memonayaj <memonayaj9864@gmail.com>
2021-12-04 12:24:45 +0530

    main

--- electrician/settings.py
+++ electrician/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'l+^+g-0b7e26k*949oj51i#m$d02n%=6@=@flm&oeq4b02u7hq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["satkar.online","www.satakar.online","127.0.0.1"]
+ALLOWED_HOSTS = ["satkar.online","www.satkar.online","127.0.0.1"]
 
 
 # Application definition
@@ -79,20 +79,20 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': 'db.sqlite3',
-    # }
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'satkaron_electrician',
-        'USER': 'satkaron_akhil786',
-        'PASSWORD': 'akhil9601657865',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
+
+    # 'default': {
+
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'satkaron_electrician',
+    #     'USER': 'satkaron_akhil786',
+    #     'PASSWORD': 'akhil9601657865',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
